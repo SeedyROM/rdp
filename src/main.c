2b94665f1c57c4da3be0a407b535f90182b43d83
Zack Kollar <rallokkcaz@gmail.com>
2021-12-10 20:35:29 -0800

    :hammer: Test adding nesteed node

--- src/main.c
+++ src/main.c
@@ -22,10 +22,12 @@ int main(int argc, char *argv[])
   ast_node *node0 = ast_node_bool(1);
   ast_node *node1 = ast_node_bool(0);
   ast_node *node2 = ast_node_null();
+  ast_node *node3 = ast_node_object();
 
   ast_node_object_append(root, node0);
   ast_node_object_append(root, node1);
   ast_node_object_append(root, node2);
+  ast_node_object_append(root, node3);
 
   ast_node *p = NULL;
   while ((p = (ast_node *)utarray_next((UT_array *)root->value, p)))
